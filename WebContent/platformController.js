8e168d595fa09933e441dd50fd1453ba74da4fa1
Brandon <brandondong604@hotmail.com>
2016-08-31 00:23:00 -0700

    Display platform logos in table

--- WebContent/platformController.js
+++ WebContent/platformController.js
@@ -1,3 +1,5 @@
+const NUM_COLUMNS = 10;
+
 angular.module("wuphf", []).controller("platformCtrl", function($scope, $http) {
 	integrationService.platforms($http, function(platforms) {
 		$scope.platforms = platforms;
@@ -5,5 +7,13 @@ angular.module("wuphf", []).controller("platformCtrl", function($scope, $http) {
 	
 	$scope.select = function(platform) {
 		$scope.selected = platform;
+	};
+	
+	$scope.split = function() {
+		var arrays = [];
+		for (var i = 0; i < $scope.platforms.length; i += NUM_COLUMNS) {
+			arrays.push($scope.platforms.slice(i, i + NUM_COLUMNS));
+		}
+		return arrays;
 	};
 });
