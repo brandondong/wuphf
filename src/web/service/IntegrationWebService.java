e2666bae28a07727cb1793516d10908f88530a1b
Brandon <brandondong604@hotmail.com>
2016-12-15 13:41:19 -0800

    Add support for async responses

--- src/web/service/IntegrationWebService.java
+++ src/web/service/IntegrationWebService.java
@@ -6,6 +6,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.MediaType;
 
 import core.model.PlatformManager;
@@ -30,8 +32,9 @@ public class IntegrationWebService {
 
 	@POST
 	@Path("/post")
-	public void postMessage(MessageIntegrationWrapper message) {
+	public void postMessage(MessageIntegrationWrapper message, @Suspended AsyncResponse response) {
 		service.postMessage(message);
+		response.resume("TODO, change this to service result");
 	}
 
 }
