2a04f9e35ea548c25158af2e339a8db17bc2a0dc
Brandon <brandondong604@hotmail.com>
2017-01-07 00:52:42 -0800

    Display logo images in integration page

--- jsx/Integrations.jsx
+++ jsx/Integrations.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import MainNavbar, {MainJumbotron} from './MainNavbar.jsx';
 import LocalStorageManager from './LocalStorageManager.js';
 import PageHeader from 'react-bootstrap/lib/PageHeader';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Image from 'react-bootstrap/lib/Image';
 import IntegrationWebService from './IntegrationWebService.js';
 
 class Integrations extends React.Component {
@@ -54,12 +57,22 @@ class NewIntegrationSection extends React.Component {
 	
 	render() {
 		let platforms = this.state.platforms.map(function (platform) {
-			return <p>{platform.label}</p>
+			let imagePath = "images/" + platform.logo;
+			return (
+				<Col xs={6} md={4} key={platform.label}>
+					<a href={platform.redirectUrl}>
+						<Image src={imagePath} width={57} height={57} thumbnail/>
+						<h3>{platform.label}</h3>
+					</a>
+				</Col>
+			);
 		});
 		return (
 			<div>
 				<PageHeader>Create a new integration</PageHeader>
-				{platforms}
+				<Row>
+					{platforms}
+				</Row>
 			</div>
 		);
 	}
