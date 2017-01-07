7e474aa0ee1d3ad73e98be654c36989e26a196fc
Brandon <brandondong604@hotmail.com>
2017-01-06 18:16:06 -0800

    Introduce post request to retrieve platform info

---
+++ jsx/IntegrationWebService.js
@@ -0,0 +1,21 @@
+const PLATFORM_URL = "/wuphf/rest/integration/platforms";
+
+export default class IntegrationWebService {
+	
+	getPlatforms() {
+		return new Promise(function(resolve, reject) {
+			let xmlhttp = new XMLHttpRequest();
+			xmlhttp.onreadystatechange = function() {
+				if (xmlhttp.readyState == 4) {
+					if (xmlhttp.status == 200) {
+						resolve(JSON.parse(xmlhttp.responseText));
+					} else {
+						reject(xmlhttp.responseText);
+					}
+				}
+			}
+			xmlhttp.open("GET", PLATFORM_URL, true);
+			xmlhttp.send();
+		});
+	}
+}
--- jsx/Integrations.jsx
+++ jsx/Integrations.jsx
@@ -3,22 +3,9 @@ import ReactDOM from 'react-dom';
 import MainNavbar, {MainJumbotron} from './MainNavbar.jsx';
 import LocalStorageManager from './LocalStorageManager.js';
 import PageHeader from 'react-bootstrap/lib/PageHeader';
+import IntegrationWebService from './IntegrationWebService.js';
 
 class Integrations extends React.Component {
-	render() {
-		return (
-			<div>
-				<MainNavbar/>
-				<MainJumbotron title="Integrations" message="Filler for now"/>
-				<div className="container">
-					<ExistingIntegrationsSection/>
-				</div>
-			</div>
-		);
-	}
-}
-
-class ExistingIntegrationsSection extends React.Component {
 	
 	constructor() {
 		super();
@@ -27,14 +14,52 @@ class ExistingIntegrationsSection extends React.Component {
 	}
 	
 	render() {
+		return (
+			<div>
+				<MainNavbar/>
+				<MainJumbotron title="Integrations" message="Filler for now"/>
+				<div className="container">
+					<ExistingIntegrationsSection integrations={this.state.integrations}/>
+					<NewIntegrationSection integrations={this.state.integrations}/>
+				</div>
+			</div>
+		);
+	}
+}
+
+class ExistingIntegrationsSection extends React.Component {
+	render() {
 		let content = "None";
-		if (this.state.integrations.length > 0) {
+		if (this.props.integrations.length > 0) {
 			content = "Got some";
 		}
 		return (
 			<div>
-				<PageHeader>Existing Integrations</PageHeader>
+				<PageHeader>Existing integrations</PageHeader>
 				{content}
+			</div>
+		);
+	}
+}
+
+class NewIntegrationSection extends React.Component {
+	
+	constructor() {
+		super();
+		this.state = {platforms: []};
+		new IntegrationWebService().getPlatforms().then((platforms) => {
+			this.setState({platforms: platforms});
+		});
+	}
+	
+	render() {
+		let platforms = this.state.platforms.map(function (platform) {
+			return <p>{platform.label}</p>
+		});
+		return (
+			<div>
+				<PageHeader>Create a new integration</PageHeader>
+				{platforms}
 			</div>
 		);
 	}
