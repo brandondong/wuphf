cc29f37728301252141344072dd0fe271aabd5ad
Brandon <brandondong604@hotmail.com>
2017-01-27 21:05:51 -0800

    Change behavior for custom contact selection

--- jsx/Messaging.jsx
+++ jsx/Messaging.jsx
@@ -7,9 +7,10 @@ import FormControl from 'react-bootstrap/lib/FormControl';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
 import HelpBlock from 'react-bootstrap/lib/HelpBlock';
 import Button from 'react-bootstrap/lib/Button';
-import DropdownButton from 'react-bootstrap/lib/DropdownButton';
-import MenuItem from 'react-bootstrap/lib/MenuItem';
 import Checkbox from 'react-bootstrap/lib/Checkbox';
+import Well from 'react-bootstrap/lib/Well';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
 
 class Messaging extends React.Component {
 	render() {
@@ -30,6 +31,8 @@ class MessagingForm extends React.Component {
 	constructor() {
 		super();
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.state = {showContacts: false};
 		let manager = new LocalStorageManager();
 		this.people = manager.getPeople()
 	}
@@ -39,27 +42,46 @@ class MessagingForm extends React.Component {
 		e.preventDefault();
 	}
 	
-	handleMenuItem(contact) {
+	handleChange(e) {
+		let show = e.target.value == "custom";
+		this.setState({showContacts: show});
+	}
+	
+	createContacts() {
+		let peopleJsx = [];
+		for (let contact in this.people) {
+			peopleJsx.push(
+				<Col xs={3} md={2} key={contact}>
+					<Checkbox inline onChange={() => this.handleToggleContact(contact)}>
+						{contact}
+					</Checkbox>
+				</Col>
+			);
+		}
+		return (
+			<Well>
+				<Row>
+				{peopleJsx}
+				</Row>
+			</Well>
+		);
+	}
+	
+	handleToggleContact(contact) {
 		console.log(contact);
 	}
 	
 	render() {
-		let peopleJsx = [];
-		for (let contact in this.people) {
-			peopleJsx.push(
-				<MenuItem key={contact} onClick={() => this.handleMenuItem(contact)}>
-					<Checkbox inline>
-						{contact}
-					</Checkbox>
-				</MenuItem>
-			);
-		}
+		let contacts = this.state.showContacts ? this.createContacts() : null;
 		return (
 			<form onSubmit={this.handleSubmit}>
 				<FormGroup>
-					<DropdownButton title="To" id="dropdown">
-						{peopleJsx}
-					</DropdownButton>
+					<ControlLabel>To</ControlLabel>
+					<FormControl componentClass="select" placeholder="select" onChange={this.handleChange}>
+						<option value="all">All contacts</option>
+						<option value="custom">Custom list of contacts</option>
+					</FormControl>
+					{contacts}
 				</FormGroup>
 				<FormGroup>
 					<ControlLabel>Subject</ControlLabel>
